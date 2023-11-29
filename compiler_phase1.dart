ee0d60b12a143cba9c98ee1e6fbf1c489bbf5c0c
ahmedatef286 <89429256+ahmedatef286@users.noreply.github.com>
2023-11-29 20:11:45 +0200

    arithmatic operations done

--- compiler_phase1.dart
+++ compiler_phase1.dart
@@ -1,1 +1,19 @@
-void main() {}
+void main() {
+  String identifierRegex = r"([a-zA-z]|[.]|[_])([a-zA-z]|[.]|[_]|[0-9])*";
+  String registerRegex =
+      r"[$](([0-9]|[12][0-9]|3[0-1])|(v[01]|a[0-3]|t[0-9]|s[0-7]|ra))";
+  String integerRegex = r"[0-9]+";
+  String commaRegex = r"(\s)*,(\s)*";
+  String arithmaticOpRegex = r"(mul|sub|add)";
+  String commandRegex = r"(\s)*" + arithmaticOpRegex + r"(\s)+";
+
+  RegExp arithmeticExpression = RegExp(r"^" +
+      commandRegex +
+      registerRegex +
+      commaRegex +
+      registerRegex +
+      commaRegex +
+      registerRegex +
+      r"(\s)*$");
+  print(arithmeticExpression.hasMatch('add \$0      , \$1, \$31    '));
+}
